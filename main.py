76b07ed1b5244e4e4b1a39ff57f692fc664bf4b5
David Conover <dconover23@weldre4.org>
2023-03-09 10:35:32 -0700

    Signed-off-by: David Conover <dconover23@weldre4.org>

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import tkinter as tk
-from weather import *
+from weatherstation import *
 
-window = tk.Tk()
-window.title("Wacky Weather Station: Hacky Prototype Edition")
-window.geometry("740x640")
+print("Wacky Weather Station")
 
-label = tk.Label(text="Enter a City")
-entry = tk.Entry(bg="lightgrey", width=50)
+while True:
+    city = get_city()
+    temperature = get_temperature(city)
 
-label.pack()
-entry.pack()
+    print("The current temperature of ", city, "is ", temperature)
 
-window.mainloop()
---
+++ weatherstation.py
@@ -0,0 +1,31 @@
+# weather.py: Contains all the important weather-accessing stuff.
+import requests
+
+
+# TODO: Figure out why API request is 401'ing
+api_key = "3ec252c41ca3ab7db6eb8f63408bed10"
+
+# "Base" URL to initialize with
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+def get_city():
+    city = input("Enter a city name: ")
+    return city
+
+def get_temperature(city):
+    # Complete URL for OpenWeatherMap API
+    complete_url = base_url + "appid=" + api_key + "&q=" + city
+
+    # Fire off request to API
+    response = requests.get(complete_url)
+
+    # The data we get back from the API
+    result = response.json()
+    print(result)
+
+    if result["cod"] != "404":
+        main = result["main"]
+        current_temp = main["temp"]
+
+        current_temperature = round((current_temp - 273.15) * 1.8 + 32)
+        return current_temperature
