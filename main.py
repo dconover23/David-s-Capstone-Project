aabebb30bb239abe6aad78bfaed13379f67c7a4a
David Conover <dconover23@weldre4.org>
2023-03-24 13:23:56 -0600

    Add wind direction, formatting changes

--- main.py
+++ main.py
@@ -2,23 +2,57 @@ import tkinter as tk
 import weatherstation
 
 window = tk.Tk()
+# Tutorial shouldn't be opened unless the user asks for it
+tutorial = tk.Toplevel(window)
+tutorial.withdraw()
 
 window.title("Wacky Weather Station")
 window.geometry("800x480")
 window.resizable(False, False)
+
+tutorial.title("Help")
+tutorial.resizable(False, False)
 
 # Create the background image
 background_image = tk.PhotoImage(file="newclouds.gif")
 background_label = tk.Label(window, image=background_image)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
+# Create the Tkinter vars so that the information display can automatically update
+tempDisplay = tk.IntVar(window, 0)
+windDirDisplay = tk.StringVar(window, "")
+# Initialized outside of try...except block to avoid UnboundLocalError
+windDir_label = tk.Label(window, text="")
+weather_label = tk.Label(window, text="")
+
+# The tutorial window that is opened when the help button is pressed.
+def openTutorial():
+    tutorialtext = tk.Label(tutorial, "Type a city into the prompt to get the temp and wind speed of that location. "
+                                      "If you want to be precise with the location, use the format [City, "
+                                      "Country/State].")
+    tutorialtext.pack()
+
+# This will submit the user's input to the API when the corresponding button is pressed. It also clears the label
+# in case it already hasn't to make space for any new city the user submits.
 def getWeatherWithEntry():
     user_input = entry.get()
+
     if user_input != "":
+        # get the temperature for the user's choice of city...
         temperature = weatherstation.get_temperature(user_input)
-        print("The current temp is", temperature)
-    else:
-        print("You need to enter a valid city.")
+        tempDisplay.set(temperature)
+        weather_label = tk.Label(window, text="")
+        weather_label = tk.Label(window, text=f"{tempDisplay.get()} degrees Fahrenheit")
+
+        # ...and do the same for wind direction.
+        windDirection = weatherstation.get_wind_direction(user_input)
+        windDirDisplay.set(windDirection)
+        windDir_label = tk.Label(window, text="")
+        windDir_label = tk.Label(window, text=f"{windDirDisplay.get()} is the current wind direction (in degrees)")
+
+        weather_label.place(x=500, y=120)
+        windDir_label.place(x=500, y=160)
+
 
 # Create the entry box for inputting city
 entry = tk.Entry(window)
--- weatherstation.py
+++ weatherstation.py
@@ -28,6 +28,7 @@ def get_temperature(city):
         return current_temperature
 
 def get_wind_direction(city):
+
     # Complete URL for OpenWeatherMap API
     complete_url = base_url + "appid=" + api_key + "&q=" + city
 
@@ -37,23 +38,11 @@ def get_wind_direction(city):
     # The data we get back from the API
     result = response.json()
 
-    # If-elif hell to get the wind direction for the servos
+    # Get wind direction for servo
     if result["cod"] != "404":
         wind = result["wind"]
         wind_direction_degrees = wind["deg"]
-        if wind_direction_degrees >= 348.75 or wind_direction_degrees < 11.25:
-            wind_direction = "N"
-        elif wind_direction_degrees >= 33.75 and wind_direction_degrees < 56.25:
-            wind_direction = "NE"
-        elif wind_direction_degrees >= 78.75 and wind_direction_degrees < 101.25:
-            wind_direction = "E"
-        elif wind_direction_degrees >= 123.75 and wind_direction_degrees < 146.25:
-            wind_direction = "SE"
-        elif wind_direction_degrees >= 168.75 and wind_direction_degrees < 191.25:
-            wind_direction = "S"
-        elif wind_direction_degrees > 202.5 and wind_direction_degrees <= 247.5:
-            wind_direction = "SW"
-        elif wind_direction_degrees >= 258.75 and wind_direction_degrees < 281.25:
-            wind_direction = "W"
-        elif wind_direction_degrees >= 303.75 and wind_direction_degrees < 326.25:
-            wind_direction = "NW"
+        wind_direction = wind_direction_degrees
+
+
+    return wind_direction
